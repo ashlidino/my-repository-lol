004a76e5587c881b996ca9f94b6ec9ab7d1972d4
ashlidino <ashlidino2017@gmail.com>
2025-09-05 15:14:06 +0300

    Create persona.js

---
+++ persona.js
@@ -0,0 +1,17 @@
+let myName="Mary"
+
+let myMood="alright"
+
+let myFavemojii="T-T"
+
+
+
+console.log("Hi! My name is " + myName)
+
+console.log("My mood right now is " + myMood)
+
+console.log("My favourite emojii is " + myFavemojii)
+
+
+
+console.log("Hi, my name is " + myName + ", I am feeling " + myMood + ". My favourite emojii is: " + myFavemojii)
